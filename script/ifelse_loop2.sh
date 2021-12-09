62633786d2b7541deabcc437b94af20799be30ea
=chander <=chander.kala15@gmail.com>
2021-12-09 12:07:19 +1100

    loop2 added

--- script/ifelse_loop2.sh
+++ script/ifelse_loop2.sh
@@ -1,8 +1,17 @@
 age=10
 name="Yashvi"
-if [ "$age" -ge 13 -a "$name = "Yashvi"];
+#if [ "$age" -ge 13 -a "$name = "Yashvi"];
+#then
+#echo "child is not teen yet"
+#else
+#echo "child is teen"
+#fi
+
+age=10
+name="Yashvi"
+if [ age <=13 ];
 then
-echo "child is not teen yet"
+echo "child is teen yet"
 else
 echo "child is teen"
 fi
