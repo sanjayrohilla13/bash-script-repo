e2533f92f5e1e506f4d3b3f7e3a693d52935e894
=chander <=chander.kala15@gmail.com>
2021-12-09 12:16:13 +1100

    updated nested-ifelse

--- nestedifelse/nested_ifelse.sh
+++ nestedifelse/nested_ifelse.sh
@@ -1,8 +1,8 @@
 age=10
 name="Yashvi"
-if [ age <=13 -a "$name = "Yashvi"];
+if [ "$age" -ge 13 -a "$name" = "Yashvi"];
 then
-echo "child is not teen yet"
+echo "child is teen yet"
 else
-echo "child is teen"
+echo "child is not teen"
 fi
